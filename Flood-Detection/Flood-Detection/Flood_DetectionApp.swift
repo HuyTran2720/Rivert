b94ffc46e83aac8035a2ba4030359cf81df21ab7
HuyTran2720 <ht2005790@gmail.com>
2026-08-06 15:57:41 +0800

    init commit

---
+++ Flood-Detection/Flood-Detection/Flood_DetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flood_DetectionApp.swift
+//  Flood-Detection
+//
+//  Created by Huy Tran on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Flood_DetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
