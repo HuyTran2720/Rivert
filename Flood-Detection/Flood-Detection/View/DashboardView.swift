b899a98695165b423e464cba8d80b2cfa07f00ae
Dhita <alfatrahdhita@gmail.com>
2026-08-14 13:48:46 +0800

    {feat} dashboard view

---
+++ Flood-Detection/Flood-Detection/View/DashboardView.swift
@@ -0,0 +1,58 @@
+//
+//  DashboardView.swift
+//  Flood-Detection
+//
+//  Created by dhita alfatrah on 13/08/26.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+                VStack(alignment: .leading, spacing: 25) {
+                    HStack {
+                        Text("Date")
+                        
+                        Spacer()
+                        
+                        Text("Location")
+                    }
+                    HStack(spacing: 15) {
+                        Text("Icon")
+                        Text("Keterangan")
+                    }
+                }
+            .padding(.horizontal, 40)
+            .padding(.top, 30)
+           
+            Spacer()
+            Text("Status")
+                .padding(.leading, 160)
+            Spacer()
+            
+            Text("Background Image")
+            
+            Spacer()
+            
+            Text("Time")
+                .padding(.bottom, 10)
+            Spacer()
+            
+            HStack {
+                Text("Water Level")
+                
+                Spacer()
+                
+                Text("Rate of Water Rise")
+            }
+            .padding(.horizontal, 40)
+            .padding(.bottom, 30)
+        }
+
+    }
+}
+
+#Preview {
+    DashboardView()
+}
